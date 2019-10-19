2a7af9c594d2571f9e05ccdfabe7e874245229a5
kyle li <kyleli@kyles-MacBook-Pro.local>
2019-10-19 23:45:47 +0100

    New

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,13 +1,13 @@
 def period(L, g):
     from math import pi 
-    T = None
+    T = ""
     L_input = isinstance(L, int)
     g_input = isinstance(g, int)
     print("L_input")
     print("g_input")
-    if L_input != Ture:
+    if L_input != True:
         print("Wrong input")
-    elif g_input != Ture:
+    elif g_input != True:
         print("Wrong input")
     else:
         T = 2 * pi * (L/g)**(1/2)
